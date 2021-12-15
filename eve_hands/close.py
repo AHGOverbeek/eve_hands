809ffffd61f503e2cbebd27aab35247eab39a912
Alex Overbeek <alex.overbeek@hotmail.com>
2021-12-15 12:39:48 +0100

    Add clsoe

--- eve_hands/close.py
+++ eve_hands/close.py
@@ -26,6 +26,7 @@ class MinimalPublisher(Node):
     def __init__(self):
         super().__init__('minimal_publisher')
 
+        # A publisher for each finger because the Bebionic driver listens to one for each
         self.publisher_thumb_r_ = self.create_publisher(HandCommand, '/bebionic/right/finger_thumb', 10)
         self.publisher_index_r_ = self.create_publisher(HandCommand, '/bebionic/right/finger_index', 10)
         self.publisher_middle_r_ = self.create_publisher(HandCommand, '/bebionic/right/finger_middle', 10)
@@ -38,7 +39,8 @@ class MinimalPublisher(Node):
         self.publisher_ring_l_ = self.create_publisher(HandCommand, '/bebionic/left/finger_ring', 10)
         self.publisher_little_l_ = self.create_publisher(HandCommand, '/bebionic/left/finger_little', 10)
 
-        timer_period = 0.1 # seconds
+        # Send every 5 seconds
+        timer_period = 5
         self.timer = self.create_timer(timer_period, self.timer_callback)
 
     def timer_callback(self):
@@ -47,14 +49,14 @@ class MinimalPublisher(Node):
         msg.speed = 255.0
         msg.force = 100.0
         
-        #  Open first
+        #  Close first
         msg.closure = 15000.0
 
         # Cannot publish at the same time (without explicit delay or time.sleep), otherwise only one is executed
         # The limit seems to be about 0.04s, only 25Hz
         delay = 0.05;
 
-        # Right open fingers
+        # Right close fingers
         self.publisher_little_r_.publish(msg)
         time.sleep(delay)
         self.publisher_ring_r_.publish(msg)
@@ -66,7 +68,7 @@ class MinimalPublisher(Node):
         self.publisher_thumb_r_.publish(msg)
         time.sleep(delay)
 
-        # Left open fingers
+        # Left close fingers
         self.publisher_thumb_l_.publish(msg)
         time.sleep(delay)
         self.publisher_index_l_.publish(msg)
@@ -78,8 +80,8 @@ class MinimalPublisher(Node):
         self.publisher_little_l_.publish(msg)
         time.sleep(delay)
 
-def main(args=None):
-    rclpy.init(args=args)
+def main():
+    rclpy.init()
 
     minimal_publisher = MinimalPublisher()
 
